1377e863fb3965a7eb22241ba8405567fb60fe11
luc1am <90805550+luc1am@users.noreply.github.com>
2022-02-10 00:41:57 -0500

    Add files via upload

--- chatbots/sketch.js
+++ chatbots/sketch.js
@@ -5,34 +5,98 @@ let data;
 let input, sendButt;
 let answer;
 let capture;
+let loopNum = 0;
 
-//load json file into sketches
+let palette = {
+  green: '#27320b',
+  brown: '#9d4006',
+  white: '#fefae0',
+  yellow: '#f5d400'
+}
+
+
+class Chef {
+  constructor(x,y,sizex,sizey, emotion, filename){
+    this.x = x;
+    this.y = y;
+    this.sizex = sizex;
+    this.sizey = sizey;
+    this.emotion = emotion; //string?
+    this.filename = filename;
+    this.img = loadImage(filename);
+  }
+  // pre(){    img = loadImage(this.filename);  }
+  display(){
+    image(this.img, this.x, this.y)
+  }
+}
+
+//press enter or press button
+
+let pics = [
+  "media/cookbotBase1.png",
+  "media/cookbotInfo1.png",
+"media/cookbotBaking1.png",
+"media/cookbotAdvice1.png",
+"media/cookbotInsult1.png",
+"media/cookbotGreeting1.png",
+"media/cookbotFood1.png",
+"media/cookbotCatchall1.png"
+];
+let mood = [
+  'base',
+  'info',
+  'bake',
+  'advice',
+    'insult',
+  'greeting',
+  'food',
+  'catchall']
+ let chefs = [];
 
 function preload(){
   data = loadJSON("data.json");
+  for (let i = 0; i<pics.length; i++){
+    chefs.push(new Chef(0,100,100,100,
+      mood[i],pics[i]));
+  }
+  // for(let i = 0; i<chefs.length; i++){
+  //   chefs[i].pre();
+  // }
+
 }
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500,380);
   //console.log(data);
+
+
   let title = createP("cookbot");
-  title.style('font-size', '20px');
-  title.position(15,0);
+  title.style('font-size', '20px')
+  title.style('color', color(palette.green));
+  title.position(8,-18);
 
   input  = createInput();
   input.position(50,50);
   input.size(300,50);
+  input.style('background-color', color(palette.yellow))
   sendButt = createButton("send");
   sendButt.position(370,85);
-  sendButt.style('background-color', color(51, 22, 117,100))
+  sendButt.style('background-color', color(palette.green))
+  sendButt.style('color', color(palette.white))
 
   sendButt.mousePressed(chitchat);
 
-  capture = createCapture(VIDEO);
-  capture.hide();
+  // capture = createCapture(VIDEO);
+  // capture.hide();
 
 }
 
+function keyTyped(){
+  if (keyCode == ENTER){
+    chitchat();
+  }
+}
 
 
 function chitchat(){
@@ -44,6 +108,7 @@ function chitchat(){
       if(inputStr.indexOf(data.brain[i].triggers[j])!== -1){
         //we have a match!
         // print("OK");
+        loopNum = i+1;
         answer = random(data.brain[i].responses);
         //print(answer);
         break loop1;
@@ -56,11 +121,20 @@ function chitchat(){
   //nested forloop
 }
 
+//brown as background
+// white border
+//green and yellow are highlights
 function draw() {
-  let width = 100;
+  background(palette.white);
+  fill(palette.brown);
+  rect(25,25,450,330);
+  noStroke();
+  fill(palette.white);
+  rect(250,150,200,150,10);
+  triangle(250,200,250,220,240,210);
+  chefs[loopNum].display();
+  fill(palette.green);
+  text(answer, 260,160, 150);
 
-  background(200,180,190);
-
-  text(answer, 100,150, 100);
-  image(capture, 370,5,width, width*capture.height/capture.width);
+  //image(capture, 370,5,width, width*capture.height/capture.width);
 }
